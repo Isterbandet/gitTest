59dfd40a86760602a5ccec5aa817d044c0d1c67c
Anton Eklöf <anton_eklof@hotmail.com>
2018-04-19 14:46:27 +0200

    vafan

---
+++ src/gittest/gitTest2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest;
+
+/*
+////BARNCH 1!!!!!!*
+ *
+ * @author Torsten
+ */
+public class gitTest2 {
+    
+    void Jagärettäpple(){
+    int i = 7899;
+    }
+    //Aaa
+}
